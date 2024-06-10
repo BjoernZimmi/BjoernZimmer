388d5068dbb84be564344c89c9b25b0140e9cd1f
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-10 13:33:57 +0200

    added service-structure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import { Environments } from "./environment.interface";
 
 export const environments: Environments = {
-    production: true,
+    production: false,
     name: 'dev',
     apiPath: 'in_progress'
 }
