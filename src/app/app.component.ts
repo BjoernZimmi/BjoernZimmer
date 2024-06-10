d1a6a01f36762e2f04fa4886eb6819193b792676
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-10 15:10:55 +0200

    added PrimeNG and startpage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppComponent {
   title = 'Bjoern_Zimmer';
+  menubarItems: MenuItem[] = [
+    {
+      label: 'Start',
+    },
+    {
+      label: 'Frontend',
+    },
+    {
+      label: 'Backend',
+    },
+  ]
 }
