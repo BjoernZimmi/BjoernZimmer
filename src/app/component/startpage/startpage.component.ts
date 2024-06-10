5617f6d2c8eab60180c0327b896706193438d524
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-10 15:10:15 +0200

    addded PrimeNG and startpage component

---
+++ src/app/component/startpage/startpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './startpage.component.html',
+  styles: ``
+})
+export class StartpageComponent {
+
+}
