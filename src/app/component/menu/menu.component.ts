a4d58d5751a8517e7b42f259b162815915080131
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-14 15:09:09 +0200

    added background images for front- and backend

--- src/app/component/menu/menu.component.ts
+++ src/app/component/menu/menu.component.ts
@@ -5,7 +5,6 @@ import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-menu',
   standalone: true,
@@ -13,67 +12,47 @@ import { CommonModule } from '@angular/common';
   templateUrl: './menu.component.html',
   styles: ``
 })
-
 export class MenuComponent implements OnInit {
-  @ViewChild('dropdownTemplate') dropdownTemplate!: TemplateRef<any>;
+  // @ViewChild('dropdownTemplate') dropdownTemplate!: TemplateRef<any>;
 
   selectedCountry!: Country;
-  selectedLanguage: 'en' | 'de' = 'en';
+  // selectedLanguage: 'en' | 'de' = 'en';
 
   menubarItems: MenuItem[] = [];
 
-  onCountrySelect(country: Country) {
-    this.selectedCountry = country;
-  }
-
   countries: Country[] = [
-    { code: 'DE', name: { en: 'Germany', de: 'Deutschland' } },
-    { code: 'EN', name: { en: 'English', de: 'Englisch' } },
+    { code: 'DE', name: { en: 'Germany', de: 'Deutschland' }, flagIcon: 'flag-icon flag-icon-de' },
+    { code: 'GB', name: { en: 'United Kingdom', de: 'Vereinigtes Königreich' }, flagIcon: 'flag-icon flag-icon-gb' },
   ];
 
-
-  get displayCountries() {
-    return this.countries.map(country => ({
-      ...country,
-      displayName: country.name[this.selectedLanguage]
-    }));
-  }
+  // get displayCountries() {
+  //   return this.countries.map(country => ({
+  //     ...country,
+  //     displayName: country.name[this.selectedLanguage],
+  //     icon: country.flagIcon
+  //   }));
+  // }
 
   ngOnInit(): void {
-    if (this.displayCountries) {
-      this.menubarItems = [
-        {
-          label: 'Start',
-          routerLink: 'start'
-        },
-        {
-          label: 'Frontend',
-          routerLink: 'frontend'
-        },
-        {
-          label: 'Backend',
-          routerLink: 'backend'
-        },
-        {
-          label: 'Country',
-          items: this.displayCountries.map(country => ({
-            label: country.displayName,
-            command: () => {
-              this.selectedCountry = country;
-              this.onCountrySelect(country);
-            }
-          }))
-        }
-      ];
-    }
+    this.menubarItems = [
+      {
+        label: 'Start',
+        routerLink: 'start'
+      },
+      {
+        label: 'Frontend',
+        routerLink: 'frontend'
+      },
+      {
+        label: 'Backend',
+        routerLink: 'backend'
+      }
+    ];
   }
-
-
 }
 
 interface Country {
   code: string;
   name: { [key: string]: string };
-
-
+  flagIcon: string;
 }
