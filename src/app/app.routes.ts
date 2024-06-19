943d488e43d9ea73c010da42b4a7ef29f4ddfbae
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-19 14:48:35 +0200

    added missing files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { BackendComponent } from './component/backend/backend.component';
 import { DatabaseComponent } from './component/database/database.component';
 import { ImpressumComponent } from './component/impressum/impressum.component';
 import { LoginComponent } from './component/login/login.component';
+import { InformationComponent } from './component/information/information.component';
 
 export const routes: Routes = [
     { path: '', component: StartpageComponent, title: 'Björn Zimmer' },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'backend', component: BackendComponent, title: 'Björn Zimmer - Backend' },
     { path: 'database', component: DatabaseComponent, title: 'Björn Zimmer - Database' },
     { path: 'impressum', component: ImpressumComponent, title: 'Björn Zimmer - Impressum' },
+    { path: 'information', component: InformationComponent, title: 'Björn Zimmer - Information' },
     { path: 'login', component: LoginComponent, title: 'Björn Zimmer - Login' },
     { path: '**', component: StartpageComponent, title: 'Björn Zimmer' },
 
--- src/app/core/services/translation/translation.service.ts
+++ src/app/core/services/translation/translation.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TranslationService {
-  constructor(public translate: TranslateService) {
-    this.translate.setDefaultLang('de');
+  browserLanguage!: string;
+
+  constructor(
+    public translate: TranslateService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    // Initialisieren Sie die Standardsprache basierend auf dem Kontext (Browser oder nicht)
+    this.initializeDefaultLanguage();
+  }
+
+  private initializeDefaultLanguage(): void {
+    let defaultLang = 'de'; // Standardsprache
+
+    // Überprüfen Sie, ob der Code im Browser ausgeführt wird
+    if (isPlatformBrowser(this.platformId)) {
+      
+      // Extrahiert den Sprachcode, z.B. 'en' aus 'en-US'
+      const browserLang = navigator.language.split('-')[0]; 
+      const supportedLanguages = ['de', 'en'];
+
+      this.browserLanguage = browserLang;
+      console.log(browserLang);
+
+
+      if (supportedLanguages.includes(browserLang)) {
+        defaultLang = browserLang;
+      }
+    }
+
+    this.translate.setDefaultLang(defaultLang);
+    this.translate.use(defaultLang);
   }
 
   useLanguage(language: string): void {
@@ -15,5 +45,9 @@ export class TranslationService {
 
   getCurrentLanguage(): string {
     return this.translate.currentLang;
+  }
+
+  setbrowserLanguage(): string {
+    return this.browserLanguage;
   }
 }
