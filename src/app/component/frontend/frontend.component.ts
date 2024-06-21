88936c9d3baa765d4a148efedc9933155d2c97ff
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-06-21 08:44:24 +0200

    change frontend and backend translation

--- src/app/component/frontend/frontend.component.ts
+++ src/app/component/frontend/frontend.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-frontend',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './frontend.component.html',
   styles: ``
 })
