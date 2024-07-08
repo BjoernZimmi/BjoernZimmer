4038c74ed72e98f3a43374890b056ba77973fcc4
Bjoern Zimmer <bjoern.zimmer@dachpc.com>
2024-07-08 14:09:02 +0200

    added privacy police

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DatabaseComponent } from './component/database/database.component';
 import { ImpressumComponent } from './component/impressum/impressum.component';
 import { LoginComponent } from './component/login/login.component';
 import { InformationComponent } from './component/information/information.component';
+import { PrivacyPolicyComponent } from './component/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: StartpageComponent, title: 'Björn Zimmer' },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'frontend', component: FrontendComponent, title: 'Björn Zimmer - Frontend' },
     { path: 'backend', component: BackendComponent, title: 'Björn Zimmer - Backend' },
     { path: 'database', component: DatabaseComponent, title: 'Björn Zimmer - Database' },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent, title: 'Björn Zimmer - Privacy policy' },
     { path: 'impressum', component: ImpressumComponent, title: 'Björn Zimmer - Impressum' },
     { path: 'information', component: InformationComponent, title: 'Björn Zimmer - Information' },
     { path: 'login', component: LoginComponent, title: 'Björn Zimmer - Login' },
